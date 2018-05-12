7f5ad81c8c126ec8d0db49bab31092b01367ac6a
RM <RM@RM-PC>
2018-05-12 19:13:10 -0400

    0.1b

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRIPTOMEX - Una nueva manera de hacer negocios</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link href="css/prettyPhoto.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet" />	
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<header>		
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="navigation">
+				<div class="container">					
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<div class="navbar-brand">
+							<a href="index.php"><h1><span>Cripto</span>Mex</h1></a>
+						</div>
+					</div>
+					
+					<div class="navbar-collapse collapse">							
+						<div class="menu">
+							<ul class="nav nav-tabs" role="tablist">
+								<li role="presentation"><a href="index.php" class="active">Inicio</a></li>
+								<li role="presentation"><a href="about.php">Sobre nosotros</a></li>
+								<li role="presentation"><a href="services.php">Servicios</a></li>								
+								<li role="presentation"><a href="portfolio.php">Productos</a></li>								
+								<li role="presentation"><a href="contact.php">Contacto</a></li>						
+                                                                <li role="presentation"><a href="login.php">Ingresar</a></li>	
+							</ul>
+						</div>
+					</div>						
+				</div>
+			</div>	
+		</nav>		
+	</header>
+	
+	<section id="main-slider" class="no-margin">
+        <div class="carousel slide">      
+            <div class="carousel-inner">
+                <div class="item active" style="background-image: url(images/slider/bg1.jpg)">
+                    <div class="container">
+                        <div class="row slide-margin">
+                            <div class="col-sm-6">
+                                <div class="carousel-content">
+                                    <h2 class="animation animated-item-1">Esto es <span>CRIPTOMEX</span></h2>
+                                    <p class="animation animated-item-2">Su vehiculo hacia la nueva economía</p>
+                                    <a class="btn-slide animation animated-item-3" href="#">Conozca más</a>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 hidden-xs animation animated-item-4">
+                                <div class="slider-img">
+                                    <img src="images/slider/img3.png" class="img-responsive">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div><!--/.item-->             
+            </div><!--/.carousel-inner-->
+        </div><!--/.carousel-->
+    </section><!--/#main-slider-->
+	
+	<div class="feature">
+		<div class="container">
+			<div class="text-center">
+				<div class="col-md-3">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" >
+						<i class="fa fa-book"></i>	
+						<h2>SERVICIO 1</h2>
+						<p>Quisque eu ante at tortor imperdiet gravida nec sed turpis phasellus.</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" >
+						<i class="fa fa-laptop"></i>	
+						<h2>SERVICIO 2</h2>
+						<p>Quisque eu ante at tortor imperdiet gravida nec sed turpis phasellus.</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms" >
+						<i class="fa fa-heart-o"></i>	
+						<h2>SERVICIO 3</h2>
+						<p>Quisque eu ante at tortor imperdiet gravida nec sed turpis phasellus.</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
+						<i class="fa fa-cloud"></i>	
+						<h2>SERVICIO 4</h2>
+						<p>Quisque eu ante at tortor imperdiet gravida nec sed turpis phasellus.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="about">
+		<div class="container">
+			<div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" >
+				<h2>¿Qué es Criptomex?</h2>
+				<img src="images/6.jpg" class="img-responsive"/>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				</p>
+			</div>
+			
+			<div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" >
+				<h2>Jamás había sido tan fácil</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget. 
+				Curabitur lacinia pellentesque libero, pulvinar tincidunt leo consectetur eget. 
+				Curabitur lacinia pellentesque libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque </p>
+			</div>
+		</div>
+	</div>
+	
+	<div class="lates">
+		<div class="container">
+			<div class="text-center">
+				<h2>Productos CRIPTOMEX</h2>
+			</div>
+			<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+				<img src="images/4.jpg" class="img-responsive"/>
+				<h3>Producto 1</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				</p>
+			</div>
+			
+			<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+				<img src="images/4.jpg" class="img-responsive"/>
+				<h3>Producto 2</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				</p>
+			</div>
+			
+			<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">				
+				<img src="images/4.jpg" class="img-responsive"/>
+				<h3>Producto 3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+				</p>
+			</div>
+		</div>
+	</div>
+	
+	<section id="partner">
+        <div class="container">
+            <div class="center wow fadeInDown">
+                <h2>Nuestos aliados</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
+            </div>    
+
+            <div class="partners">
+                <ul>
+                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" src="images/partners/partner1.png"></a></li>
+                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" src="images/partners/partner2.png"></a></li>
+                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms" src="images/partners/partner3.png"></a></li>
+                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" src="images/partners/partner4.png"></a></li>
+                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1500ms" src="images/partners/partner5.png"></a></li>
+                </ul>
+            </div>        
+        </div><!--/.container-->
+    </section><!--/#partner-->
+	
+	<section id="conatcat-info">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="media contact-info wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="pull-left">
+                            <i class="fa fa-phone"></i>
+                        </div>
+                        <div class="media-body">
+                            <h2>¿Te gustaría contactarnos?</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation +0123 456 70 80</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--/.container-->    
+    </section><!--/#conatcat-info-->
+	
+	<footer>
+		<div class="footer">
+			<div class="container">
+				<div class="social-icon">
+					<div class="col-md-4">
+						<ul class="social-network">
+							<li><a href="#" class="fb tool-tip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#" class="twitter tool-tip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#" class="gplus tool-tip" title="Google Plus"><i class="fa fa-google-plus"></i></a></li>
+							<li><a href="#" class="linkedin tool-tip" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+							<li><a href="#" class="ytube tool-tip" title="You Tube"><i class="fa fa-youtube-play"></i></a></li>
+						</ul>	
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-md-offset-4">
+					<div class="copyright">
+						&copy; Mayo 2018 <a target="_blank" href="http://www.demente.com.ve/" title="DeMente">DeMente C.A</a>.
+					</div>
+                    
+				</div>						
+			</div>
+			
+			<div class="pull-right">
+				<a href="#home" class="scrollup"><i class="fa fa-angle-up fa-3x"></i></a>
+			</div>		
+		</div>
+	</footer>
+
+	
+	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery-2.1.1.min.js"></script>	
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>  
+	<script src="js/wow.min.js"></script>
+	<script src="js/functions.js"></script>
+	
+  </body>
+</html>
